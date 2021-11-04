206318636e819fe727b2424f930525fd29a7d8b2
codeorafk <63441848+codeorafk@users.noreply.github.com>
2021-11-04 07:10:30 +0700

    update 4/11/2021

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
-// double click on the heart icon
-$(".fa-heart").dblclick(function () {
-    $(".notification-bubble").show(400);
-});
+// Get the modal
+var modal = document.getElementByClassName("myModal");
 
-$(document).on("scroll", function () {
-if ($(document).scrollTop() > 50) {
-    $(".navigation").addClass("shrink");
-} else {
-    $(".navigation").removeClass("shrink");
+// Get the button that opens the modal
+var btn = document.getElementByClassName("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+for(x in btn)
+{
+    x.onclick = function() {
+    modal.style.display = "block";
+    }
 }
-});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
